e5a9fef74d5aadab59894ddef29da234c9fe8642
oreys <83587222+oreys@users.noreply.github.com>
2023-08-31 20:05:13 +0200

    p

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="footer">
         <div class="footer-line">
             <a href="impressum.php" style="text-decoration: underline">Impressum</a>
-            Copyright ©2023 landslaw.de
+            <b>Copyright ©2023 landslaw.de</b>
             <a href="datenschutzerklaerung.php" style="text-decoration: underline">Datenschutzerklärung</a>
         </div>
     </footer>
--- header.php
+++ header.php
@@ -1,4 +1,8 @@
-    <header class="header">
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+    
+	<header class="header">
         <div class="logo">
 			<a href="index.php">
 				<img src="images/logo.jpg" alt="Logo">
